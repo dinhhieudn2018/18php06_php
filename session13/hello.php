5bb516053ec870e666530d9252b6e1f74d2689c1
Long Phat <volongphat@gmail.com>
2018-11-22 19:49:01 +0700

    thanh cong

---
+++ session13/hello.php
@@ -0,0 +1,12 @@
+<?php
+	//Comment trong PHP
+	echo "Hello PHP";
+	// Quy tac dat ten bien
+	// Gom chu cai hoa hoac thuong hoac so va dau gach duoi
+	// Khong bat dau bang so
+	// Theo quy tac camel
+	// Bang tieng anh
+	// Co y nghia
+	$a = 5;
+	// echo $b;
+?>
